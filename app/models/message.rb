a827168e80279e7db14131fb03c03041c2bfde7e
TechMura <mura.sk.katu@gmail.com>
2020-10-04 12:38:09 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
+  # imageファイルとmessagesテーブルを紐づける。messagesテーブルにカラムを追加する必要はない。
+  has_one_attached :image
 
   validates :content, presence: true
 end
